a8ae5d100e0876543d57df7d9d89519aafb7edbd
user111b15120 <130130410+user111b15120@users.noreply.github.com>
2023-12-22 11:43:21 +0800

    Create Ex05.java

---
+++ Ex05.java
@@ -0,0 +1,8 @@
+public class Ex05 {
+    public static void main(String[] args){
+        int num = 5;
+        int[] d={3, 0, 0, 1};
+        for(int i=0;i<d.length;i++)
+        System.out.println(num + "/" + d[i] + "= " + num/d[i]);
+    }
+}
